79cacc81f61b928fcba47cc8f81b7dd85dac490c
jakson lopes <jacksonlopes948@gmail.com>
2022-10-03 08:57:23 -0300

    modulo criteriaQuery

---
+++ src/main/java/com/algaworks/algafood/domain/infrastructi/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,32 @@
+package com.algaworks.algafood.domain.infrastructi.repository;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestalranreRepositoryQueries;
+import org.springframework.stereotype.Repository;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Repository
+public class RestauranteRepositoryImpl implements RestalranreRepositoryQueries {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public List<Restaurante> find(String nome,
+                                  BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+
+        CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+        criteria.from(Restaurante.class);
+
+        TypedQuery<Restaurante> query = manager.createQuery(criteria);
+        return query.getResultList();
+    }
+
+}
---
+++ src/main/java/com/algaworks/algafood/domain/repository/RestalranreRepositoryQueries.java
@@ -0,0 +1,11 @@
+package com.algaworks.algafood.domain.repository;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface RestalranreRepositoryQueries {
+    List<Restaurante> find(String nome, BigDecimal taxaFrteInicial,
+                           BigDecimal taxaFreteFinal);
+}
