4fae8791c4aa667a1211e509c875290e53c96030
jakson lopes <jacksonlopes948@gmail.com>
2022-10-04 06:51:22 -0300

    modulo criteriaQuery

--- src/main/java/com/algaworks/algafood/domain/infrastructi/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/domain/infrastructi/repository/RestauranteRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.domain.infrastructi.repository;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestalranreRepositoryQueries;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -12,6 +13,7 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -28,15 +30,21 @@ public class RestauranteRepositoryImpl implements RestalranreRepositoryQueries {
         CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
         Root<Restaurante> root = criteria.from(Restaurante.class);
 
-        Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%");
+        var predicates = new ArrayList<Predicate>();
 
-        Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFreteInicial"), "%" + taxaFreteInicial + "%");
+        if(StringUtils.hasText(nome)) {
+            predicates.add(builder.like(root.get("nome"), "%" + nome + "%")) ;
+        }
 
-        Predicate taxaFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFreteFinal"), "%" + taxaFreteFinal + "%");
-
+        if(taxaFreteInicial != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFreteInicial"), "%" + taxaFreteInicial + "%"));
+        }
+        if (taxaFreteFinal != null) {
+            predicates.add (builder.lessThanOrEqualTo(root.get("taxaFreteFinal"), "%" + taxaFreteFinal + "%"));
+        }
         TypedQuery<Restaurante> query = manager.createQuery(criteria);
 
-        criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
+        criteria.where(predicates.toArray(new Predicate[0]));
 
         return query.getResultList();
     }
