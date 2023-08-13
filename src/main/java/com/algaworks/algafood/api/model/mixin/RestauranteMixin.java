82aeddbe9b328f5ec6062ecb5a1d625ec7559279
Jakson.lopes <jacksonlopes948@gmail.com>
2023-08-13 16:08:53 -0300

    Configurando aplicação para receber e devolver datas em UTC

--- src/main/java/com/algaworks/algafood/api/model/mixin/RestauranteMixin.java
+++ src/main/java/com/algaworks/algafood/api/model/mixin/RestauranteMixin.java
@@ -1,14 +1,14 @@
 package com.algaworks.algafood.api.model.mixin;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Endereco;
 import com.algaworks.algafood.domain.model.FormaPagamento;
 import com.algaworks.algafood.domain.model.Produto;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.time.OffsetDateTime;
+import java.util.List;
 
 public abstract class RestauranteMixin {
 
@@ -19,12 +19,12 @@ public abstract class RestauranteMixin {
 	private Endereco endereco;
 	
 	@JsonIgnore
-	private LocalDateTime dataCadastro;
+	private OffsetDateTime dataCadastro;
 	
-	@JsonIgnore
-	private LocalDateTime dataAtualizacao;
+//	@JsonIgnore
+	private OffsetDateTime dataAtualizacao;
 	
-	@JsonIgnore
+//	@JsonIgnore
 	private List<FormaPagamento> formasPagamento;
 	
 	@JsonIgnore
